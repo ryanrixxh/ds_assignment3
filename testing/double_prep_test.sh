2299f4805553c86462b375fa0649c8e1157697ee
ryanrixxh <ryanmbr@protonmail.com>
2021-10-27 06:52:54 +1030

    automatic testing for a retry working

--- testing/double_prep_test.sh
+++ testing/double_prep_test.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+#tests whether another prepare will carry the value on the previous and vote for an already reached consensus
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 echo Starting test ...
 cd ..
-parallel -u -k ::: 'java Member 2' 'java Member 3' 'java Member 4' 'java Member 5' 'java Member 6' 'java Member 8' 'java Member 9' &
+parallel -u -k ::: 'java Member 2 late' 'java Member 3 medium' 'java Member 4 medium' 'java Member 5 medium' 'java Member 6 medium' 'java Member 7 medium' 'java Member 8 medium' 'java Member 9 medium' &
 sleep 2
-java Member 1 prepare > testing/outputs/double_prep_output_M1.txt &
+java Member 1 immediate prepare > testing/outputs/double_prep_output_M1.txt &
 sleep 2
-java Member 7 prepare > testing/outputs/double_prep_output_M7.txt
+java Member 7 medium prepare > testing/outputs/double_prep_output_M7.txt
 wait
---
+++ testing/prep_retry_test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#tests whether a member will retry with a higher id number and become sucessful on the second run
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
+echo Starting test ...
+cd ..
+parallel -u -k ::: 'java Member 2 late' 'java Member 3 medium' 'java Member 4 medium' 'java Member 5 medium' 'java Member 6 medium' 'java Member 8 medium' 'java Member 9 medium' &
+sleep 2
+java Member 7 medium prepare > testing/outputs/prep_retry_output_M7.txt &
+sleep 2
+java Member 1 immediate prepare > testing/outputs/prep_retry_output_M1.txt
+wait
